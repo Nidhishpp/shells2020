f957b9450f35da7407054867bb1fd1d717e474cb
Nidhishpp <nidhishpp.pp@gmail.com>
2020-02-11 17:22:32 +0530

    change

---
+++ includes/about.php
@@ -0,0 +1,71 @@
+<section class="section section-lg">
+    <div class="container">
+        <div class="row row-grid align-items-center">
+            <div class="col-md-6 order-md-2">
+                <img src="assets/img/theme/promo-1.png" class="img-fluid floating">
+            </div>
+            <div class="col-md-6 order-md-1">
+                <div class="pr-md-5">
+                    <h3>About <strong>Shells</strong> </h3>
+                    <p class="text-justify"><strong>Shells</strong>, A National Level Inter-Collegiate IT fest
+                        is a fusion of
+                        technology and
+                        accomplishments. It is an endeavour to achieve the College's vision of preparing its
+                        students to become future leaders by providing them Excellence through Education,
+                        Exposure and Experience. The Enthusiastic students of KERNEL (Kristu Jayanti Education
+                        Resource Networking Enhanced Learning) hereby presents Shells 2k19. Come exhibit your
+                        ability, and discover your inexhaustible potential and technical expertise.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section bg-secondary">
+    <div class="container">
+        <div class="row row-grid align-items-center">
+            <div class="col-md-6">
+                <img src="assets/img/theme/promo-1.png" class="img-fluid floating">
+            </div>
+            <div class="col-md-6">
+                <div class="pl-md-5">
+                    <h3>About <strong>College</strong></h3>
+                    <p class="text-justify">Kristu Jayanti College, founded in 1999, is managed by "BODHI
+                        NIKETAN TRUST", formed by the
+                        members of St. Joseph Province of the Carmelites of Mary Immaculate (CMI). The college
+                        is
+                        affiliated to Bangalore University and is reaccredited with highest grade 'A' by NAAC in
+                        Second Cycle of Accreditation. The college is recognized by UGC under the category 2(f)
+                        &
+                        12(B). The college was accorded autonomous status from 2013 by the University Grants
+                        Commission, Government of Karnataka & the Bangalore University.</p>
+                    <a href="https://kristujayanti.edu.in/" class="font-weight-bold text-warning mt-5">More
+                        About
+                        College</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="section section-lg">
+    <div class="container">
+        <div class="row row-grid align-items-center">
+            <div class="col-md-6 order-md-2">
+                <img src="assets/img/theme/promo-1.png" class="img-fluid floating">
+            </div>
+            <div class="col-md-6 order-md-1">
+                <div class="pr-md-5">
+                    <h3>About <strong>Department</strong> </h3>
+                    <p class="text-justify">Kristu Jayanti College offers MCA & MSc Computer science with the
+                        intention to produce
+                        competitive computer professionals in tune with advanced technical concepts.The main
+                        objective of the programme is to develop the desire to pursue continued professional
+                        development by seeking out knowledge and experience beyond the classroom.The focused
+                        training given on these topics helps the students to adapt themselves to the growing
+                        demand of modern IT Industry. Students work in an integrated environment that enables
+                        them to access the resources effectively for their academic and research oriented
+                        purposes.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ includes/script.php
@@ -0,0 +1,11 @@
+<!-- Core -->
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="assets/vendor/popper/popper.min.js"></script>
+<script src="assets/vendor/bootstrap/bootstrap.min.js"></script>
+<script src="assets/vendor/headroom/headroom.min.js"></script>
+<!-- Plugins -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tilt.js/1.2.1/tilt.jquery.min.js"></script>
+<!-- Argon JS -->
+<script src="assets/js/argon.js?v=1.1.1"></script>
+<!-- Custom -->
+<script src="assets/js/custom.js"></script>

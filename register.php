db8617fc3fb7d56d33d5a162a79750ac517268e7
Nikhil Hari <nikhil_hari@live.com>
2020-02-14 21:39:29 +0530

    Touch register

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Start your development with a Design System for Bootstrap 4.">
+    <meta name="author" content="Creative Tim">
+    <title>Register | Shells - Inter-Collegiate Fest</title>
+    <?php include('includes/head.php'); ?>
+</head>
+
+
+<body>
+    <?php include('includes/header.php'); ?>
+    <main>
+        <section class="section bg-secondary pb-lg-xl pt-lg-xl pt-6" id="events">
+            <div class="container">
+                <div class="row justify-content-center text-center mb-md">
+                    <div class="col-lg-8">
+                        <h2 class="display-3">Register <strong>Now</strong></h2>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <?php include('includes/footer.php'); ?>
+    <?php include('includes/script.php'); ?>
+</body>
+
+</html>
